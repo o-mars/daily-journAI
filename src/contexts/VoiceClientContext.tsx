ba127b3de51b628848e1edf6f3666624535f4a35
Omar Sherwani <ohm.mars@protonmail.com>
2024-11-27 17:49:21 -0500

    new anon users should auto start their first convo

--- src/contexts/VoiceClientContext.tsx
+++ src/contexts/VoiceClientContext.tsx
@@ -84,6 +84,34 @@ export const VoiceClientProvider: React.FC<{ children: React.ReactNode }> = ({ c
   const [isLoading, setIsLoading] = useState(false);
   const [isMicEnabled, setIsMicEnabled] = useState(true);
   const [isSpeakerEnabled, setIsSpeakerEnabled] = useState(true);
+  const [hasConnectedOnce, setHasConnectedOnce] = useState(false);
+
+  const disconnect = useCallback(() => {
+    if (voiceClient && voiceClient.connected) voiceClient.disconnect();
+    setIsStarted(false);
+  }, [voiceClient]);
+
+  const connect = useCallback(async () => {
+    if (!voiceClient) return;
+
+    try {
+      setIsLoading(true);
+      await voiceClient.connect();
+      setIsStarted(true);
+    } catch (e) {
+      console.error((e as Error).message || "Unknown error occurred");
+      disconnect();
+    } finally {
+      setIsLoading(false);
+    }
+  }, [disconnect, voiceClient]);
+
+  useEffect(() => {
+    if (voiceClient && user && user.isNewUser && user.profile.isAnonymous && !hasConnectedOnce) {
+      connect();
+      setHasConnectedOnce(true);
+    }
+  }, [user, voiceClient, connect, hasConnectedOnce]);
 
   useEffect(() => {
     if (voiceClient && voiceClient.connected) {
@@ -171,27 +199,6 @@ export const VoiceClientProvider: React.FC<{ children: React.ReactNode }> = ({ c
       }
     };
   }, [user]);
-
-  const disconnect = useCallback(() => {
-    if (voiceClient && voiceClient.connected) voiceClient.disconnect();
-    setIsStarted(false);
-  }, [voiceClient]);
-
-  const connect = useCallback(async () => {
-    if (!voiceClient) return;
-
-    try {
-      setIsLoading(true);
-      await voiceClient.connect();
-      setIsStarted(true);
-    } catch (e) {
-      console.error((e as Error).message || "Unknown error occurred");
-      disconnect();
-    } finally {
-      setIsLoading(false);
-    }
-  }, [disconnect, voiceClient]);
-
 
   return (
     <VoiceClientContext.Provider value={{
