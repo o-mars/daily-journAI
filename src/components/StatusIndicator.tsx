247a571a02eeb9a9fc0baf89e1008c50517b6b59
Omar Sherwani <ohm.mars@protonmail.com>
2024-12-10 03:12:15 -0500

    give loading status indicator a loading spinner

--- src/components/StatusIndicator.tsx
+++ src/components/StatusIndicator.tsx
@@ -98,7 +98,7 @@ const StatusIndicator = React.forwardRef<StatusIndicatorHandle, Props>(function 
 
   return (
     <div
-      className={`status-indicator w-full rounded-md bg-transparent ${
+      className={`flex flex-rowstatus-indicator w-full rounded-md bg-transparent ${
         currentMessage.type === "loading"
           ? "text-gray-500"
           : currentMessage.type === "success"
@@ -110,6 +110,12 @@ const StatusIndicator = React.forwardRef<StatusIndicatorHandle, Props>(function 
         currentMessage ? "opacity-100" : "opacity-0"
       } ${className}`}
     >
+      {currentMessage.type === "loading" && (
+        <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+          <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+      )}
       {currentMessage.text}
     </div>
   );
